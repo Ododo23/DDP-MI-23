d491850fdcb9ea55f34791f8b0cf71952c1169cc
Ododo23 <achmadwspd008@gmail.com>
2023-09-15 12:42:14 +0700

    02 Masukan dan Keluaran

---
+++ 02 Masukan dan Keluaran/odo_soal5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    string str;
+    getline(cin, str);
+    cout << str <<endl;
+    
+    string str100;
+    getline(cin, str);
+    cout << str<<endl;
+    
+    string str200;
+    getline(cin, str);
+    cout << str<<endl;
+    
+    string str300;
+    getline(cin, str);
+    cout << str<<endl;
+    
+    string str400;
+    getline(cin, str);
+    cout << str;
+    
+    return 0;
+}
