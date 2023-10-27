291c7c29996cc33d2d9c104e4eb34cb173ef1639
Ododo23 <achmadwspd008@gmail.com>
2023-10-27 09:06:47 +0700

    06 fungsi

---
+++ 05 Array/array2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    // membuat array kosong
+    int nilai[5];
+
+    // mengisi array
+    nilai[0] =
+    nilai[1]
+    nilai[2]
+    nilai[3]
+    nilai[4]
+
+
+
+
+
+
+
+
+
+
+
+
+}
